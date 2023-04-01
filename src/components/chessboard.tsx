ac901f2a251835c9e62dc24e18c9d62c0248b71c
Wanirur <235861@edu.p.lodz.pl>
2023-04-01 19:08:05 +0200

    fix chesspieces aspect ratio

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -1,40 +1,61 @@
 import { useEffect, useState } from "react";
-import { type Tile, type Piece, initBoard } from "~/utils/pieces";
+import { type Tile, pieceImages, initBoard } from "~/utils/pieces";
+import Image from "next/image";
 
 const Chessboard: React.FC = () => {
   const [tiles, setTiles] = useState<Tile[]>();
-  const [isPlayerWhite, setIsPlayerWhite] = useState<boolean>(false);
+  const [isPlayerWhite, setIsPlayerWhite] = useState<boolean>(true);
   useEffect(() => {
-    const pieces = initBoard()
+    const pieces = initBoard();
 
-    if (!isPlayerWhite) {
+    if (isPlayerWhite) {
       pieces.reverse();
     }
 
     setTiles(pieces);
-    console.log(pieces)
+    console.log(pieces);
   }, [isPlayerWhite]);
 
   return (
-      <div className="container grid h-max w-max grid-cols-8 gap-0">
-        {tiles &&
-          tiles.map((tile, index) => {
-            let isWhite = true;
-            if (index % 2) {
-              isWhite = false;
-            }
+    <div className="container grid h-max w-max grid-cols-8 gap-0">
+      {tiles &&
+        tiles.map((tile, index) => {
+          let isWhite = true;
+          if (index % 2) {
+            isWhite = false;
+          }
 
-            if (Math.floor(index / 8) % 2) {
-              isWhite = !isWhite;
-            }
+          if (Math.floor(index / 8) % 2) {
+            isWhite = !isWhite;
+          }
 
-            if (isWhite) {
-              return <div key={index} className="h-20 w-20 bg-white"></div>;
-            } else {
-              return <div key={index} className="h-20 w-20 bg-green-500"></div>;
-            }
-          })}
-      </div>
+          if (isWhite) {
+            return (
+              <div key={index} className="relative h-20 w-20 bg-white">
+                {tile && pieceImages.get(tile) && (
+                  <Image
+                    src={pieceImages.get(tile) as string}
+                    alt={tile.pieceType}
+                    fill
+                  ></Image>
+                )}
+              </div>
+            );
+          } else {
+            return (
+              <div key={index} className="relative h-20 w-20 bg-green-500">
+                {tile && pieceImages.get(tile) && (
+                  <Image
+                    src={pieceImages.get(tile) as string}
+                    alt={tile.pieceType}
+                    fill
+                  ></Image>
+                )}
+              </div>
+            );
+          }
+        })}
+    </div>
   );
 };
 
--- src/utils/pieces.ts
+++ src/utils/pieces.ts
@@ -11,24 +11,39 @@ export type Piece = {
   pieceType: (typeof PieceTypes)[number];
 };
 
+const whiteRook = { pieceType: "Rook", color: "white" } as Piece;
+const blackRook = { ...whiteRook, color: "black" } as Piece;
+const whiteBishop = { pieceType: "Bishop", color: "white" } as Piece;
+const blackBishop = { ...whiteBishop, color: "black" } as Piece;
+const whiteKnight = { pieceType: "Knight", color: "white" } as Piece;
+const blackKnight = { ...whiteKnight, color: "black" } as Piece;
+const whiteKing = { pieceType: "King", color: "white" } as Piece;
+const blackKing = { ...whiteKing, color: "black" } as Piece;
+const whiteQueen = { pieceType: "Queen", color: "white" } as Piece;
+const blackQueen = { ...whiteQueen, color: "black" } as Piece;
+const whitePawn = { pieceType: "Pawn", color: "white" } as Piece;
+const blackPawn = { ...whitePawn, color: "black" } as Piece;
+
 export type Tile = Piece | null;
+
+const pieces = new Map<Piece, string>();
+pieces.set(whiteRook, "/white_rook.svg");
+pieces.set(blackRook, "/black_rook.svg");
+pieces.set(whiteBishop, "/white_bishop.svg");
+pieces.set(blackBishop, "/black_bishop.svg");
+pieces.set(whiteKnight, "/white_knight.svg");
+pieces.set(blackKnight, "/black_knight.svg");
+pieces.set(whiteKing, "/white_king.svg");
+pieces.set(blackKing, "/black_king.svg");
+pieces.set(whiteQueen, "/white_queen.svg");
+pieces.set(blackQueen, "/black_queen.svg");
+pieces.set(whitePawn, "/white_pawn.svg");
+pieces.set(blackPawn, "/black_pawn.svg");
+export const pieceImages = pieces; 
 
 export const initBoard = (): Tile[] => {
   let board = new Array(64) as Tile[];
   board = board.fill(null, 0, 64);
-
-  const whiteRook = { pieceType: "Rook", color: "white" } as Piece;
-  const blackRook = { ...whiteRook, color: "black" } as Piece;
-  const whiteBishop = { pieceType: "Bishop", color: "white" } as Piece;
-  const blackBishop = { ...whiteBishop, color: "black" } as Piece;
-  const whiteKnight = { pieceType: "Knight", color: "white" } as Piece;
-  const blackKnight = { ...whiteKnight, color: "black" } as Piece;
-  const whiteKing = { pieceType: "King", color: "white" } as Piece;
-  const blackKing = { ...whiteKing, color: "black" } as Piece;
-  const whiteQueen = { pieceType: "Queen", color: "white" } as Piece;
-  const blackQueen = { ...whiteQueen, color: "black" } as Piece;
-  const whitePawn = { pieceType: "Pawn", color: "white" } as Piece;
-  const blackPawn = { ...whitePawn, color: "black" } as Piece;
 
   board[0] = board[7] = whiteRook;
   board[1] = board[6] = whiteKnight;
