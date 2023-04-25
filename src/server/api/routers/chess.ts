811ed9688d3da0d8a6d8e5b840cf110a2cf09158
Wanirur <vaniloor@gmail.com>
2023-04-25 21:20:59 +0200

    trigger pusher event on chess move

--- src/server/api/routers/chess.ts
+++ src/server/api/routers/chess.ts
@@ -13,80 +13,69 @@ import { TRPCError } from "@trpc/server";
 export const chessgameRouter = createTRPCRouter({
   queueUp: publicProcedure
     .input(z.object({ timeControl: z.number().min(30).max(180) }))
-    .mutation(
-      async ({
-        ctx,
-        input,
-      }): Promise<
-        | {
-            uuid: string;
-            gameStarted: boolean;
+    .mutation(async ({ ctx, input }) => {
+      let tier = "guest" as RatingTier;
+      let id = "guest";
+
+      if (ctx.session) {
+        id = ctx.session.user.id;
+        try {
+          const rating = await ctx.prisma.user
+            .findUniqueOrThrow({
+              where: {
+                id: id,
+              },
+            })
+            .then((user) => user?.rating);
+          if (rating !== null) {
+            tier = resolveRatingToTier(rating);
           }
-        | undefined
-      > => {
-        let tier = "guest" as RatingTier;
-        let id = "guest";
-
-        if (ctx.session) {
-          id = ctx.session.user.id;
-          try {
-            const rating = await ctx.prisma.user
-              .findUniqueOrThrow({
-                where: {
-                  id: id,
-                },
-              })
-              .then((user) => user?.rating);
-            if (rating !== null) {
-              tier = resolveRatingToTier(rating);
-            }
-          } catch (e) {
-            console.error("no user with id: " + id + " in db");
-          }
-        }
-
-        const playerId = id === "guest" ? id : Number.parseInt(id);
-
-        if (playersWaitingForMatch.has(tier)) {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          const queue = playersWaitingForMatch.get(tier)!;
-          if (queue.length > 0) {
-            if (
-              playerId != "guest" &&
-              queue[queue.length - 1]?.white.id === playerId
-            ) {
-              throw new TRPCError({
-                code: "BAD_REQUEST",
-                message: "You are already in queue. Multiple tabs open?",
-              });
-            }
-            const game = queue.pop();
-            if (!game) {
-              throw new TRPCError({
-                code: "INTERNAL_SERVER_ERROR",
-                message:
-                  "Game you tried to join ceased to exist. Queue up again.",
-              });
-            }
-
-            game.black = { id: playerId, secondsLeft: input.timeControl };
-            matches.set(game.id, game);
-            await pusher.trigger(game.id, "match_start", { matchId: game.id });
-            return {
-              uuid: game.id,
-              gameStarted: true,
-            };
-          } else {
-            const game = new Game(playerId, input.timeControl);
-            queue.push(game);
-            return {
-              uuid: game.id,
-              gameStarted: false,
-            };
-          }
+        } catch (e) {
+          console.error("no user with id: " + id + " in db");
         }
       }
-    ),
+
+      const playerId = id === "guest" ? id : Number.parseInt(id);
+
+      if (playersWaitingForMatch.has(tier)) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const queue = playersWaitingForMatch.get(tier)!;
+        if (queue.length > 0) {
+          if (
+            playerId != "guest" &&
+            queue[queue.length - 1]?.white.id === playerId
+          ) {
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: "You are already in queue. Multiple tabs open?",
+            });
+          }
+          const game = queue.pop();
+          if (!game) {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message:
+                "Game you tried to join ceased to exist. Queue up again.",
+            });
+          }
+
+          game.black = { id: playerId, secondsLeft: input.timeControl };
+          matches.set(game.id, game);
+          await pusher.trigger(game.id, "match_start", { matchId: game.id });
+          return {
+            uuid: game.id,
+            gameStarted: true,
+          };
+        } else {
+          const game = new Game(playerId, input.timeControl);
+          queue.push(game);
+          return {
+            uuid: game.id,
+            gameStarted: false,
+          };
+        }
+      }
+    }),
   movePiece: protectedProcedure
     .input(
       z.object({
@@ -96,7 +85,7 @@ export const chessgameRouter = createTRPCRouter({
         secondsUsed: z.number().min(30).max(180),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       const user = ctx.session.user;
       const match = matches.get(input.uuid);
       if (!match) {
@@ -112,13 +101,17 @@ export const chessgameRouter = createTRPCRouter({
         });
       }
 
-      try { 
-        match.move(input.fromTile, input.toTile)
-      } catch(e) { 
+      try {
+        match.move(input.fromTile, input.toTile);
+        await pusher.trigger(match.id, "move_made", {
+          fromTile: input.fromTile,
+          toTile: input.toTile,
+        });
+      } catch (e) {
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: "Invalid move"
-        })
+          message: "Invalid move",
+        });
       }
     }),
 });
