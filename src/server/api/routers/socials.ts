0433513134cbd6111cc3cb58074fc52d89d036c3
Wanirur <vaniloor@gmail.com>
2023-06-08 19:32:49 +0200

    trpc queries for fetching player data for profile

--- src/server/api/routers/socials.ts
+++ src/server/api/routers/socials.ts
@@ -21,4 +21,80 @@ export const socialsRouter = createTRPCRouter({
 
       return player.rating;
     }),
+
+  getPlayerAccountCreationDate: publicProcedure
+    .input(z.object({ playerId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const player = await ctx.prisma.user.findFirst({
+        where: {
+          id: input.playerId,
+        },
+      });
+
+      if (!player) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "No player with provided id found",
+        });
+      }
+
+      return player.createdAt;
+    }),
+  getGamesCount: publicProcedure
+    .input(z.object({ playerId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const count = await ctx.prisma.gameToUser.count({
+        where: {
+          userId: input.playerId,
+        },
+      });
+
+      return count;
+    }),
+  getRecentGames: publicProcedure
+    .input(z.object({ playerId: z.string(), cursor: z.number().nullish() }))
+    .query(async ({ ctx, input }) => {
+      const LIMIT = 10;
+
+      const games = await ctx.prisma.game.findMany({
+        take: -(LIMIT + 1),
+        include: {
+          gameToUsers: {
+            select: {
+              color: true,
+              currentRating: true,
+              user: {
+                select: {
+                  id: true,
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+        where: {
+          gameToUsers: {
+            some: {
+              userId: input.playerId,
+            },
+          },
+        },
+        cursor: {
+          id: input.cursor ?? 1,
+        },
+        orderBy: {
+          finishedAt: "desc",
+        },
+      });
+
+      let nextCursor;
+      if (games.length > LIMIT) {
+        nextCursor = games.pop()?.id;
+      }
+
+      return {
+        games,
+        nextCursor,
+      };
+    }),
 });
