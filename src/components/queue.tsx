6df27143132981050f1da1ddec531b76ea8b9231
Wanirur <vaniloor@gmail.com>
2023-06-12 02:52:50 +0200

    add queue cancel button

--- src/components/queue.tsx
+++ src/components/queue.tsx
@@ -1,10 +1,15 @@
 import { Search } from "lucide-react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 import pusherClient from "~/utils/pusherClient";
 
-const QueueDisplay: React.FC<{ gameId: string }> = ({ gameId }) => {
+const QueueDisplay: React.FC<{
+  gameId: string;
+  setIsInQueue: Dispatch<SetStateAction<boolean>>;
+}> = ({ gameId, setIsInQueue }) => {
   const router = useRouter();
+
+  const [isQueueLong, setIsQueueLong] = useState<boolean>(false);
 
   useEffect(() => {
     const onStart = (data: { matchId: string; timeControl: number }) => {
@@ -19,10 +24,47 @@ const QueueDisplay: React.FC<{ gameId: string }> = ({ gameId }) => {
     };
   }, [gameId, router]);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsQueueLong(true);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
-    <div className="flex h-8 w-max items-center bg-neutral-900 font-os text-white">
-      <Search className="m-2 h-5 w-5 animate-pulse fill-neutral-400"></Search>{" "}
-      Looking for opponent...
+    <div className="flex h-96 w-72 flex-col items-center justify-center gap-4 bg-neutral-700  p-5 font-os text-white">
+      <div className="flex w-full items-center justify-center">
+        <Search className="m-2 h-8 w-8 animate-pulse "></Search>
+        Looking for opponent...
+      </div>
+
+      <div className="my-4 h-24 w-full">
+        {isQueueLong && (
+          <span>
+            Looks like there are no opponents currently available in your rating
+            range. Consider playing a bot instead.
+          </span>
+        )}
+      </div>
+
+      <button
+        className={`h-12 w-24 rounded-md bg-green-700 ${
+          isQueueLong ? "animate-pulse" : ""
+        }`}
+      >
+        play a bot
+      </button>
+      <button
+        className="h-12 w-24 rounded-md bg-red-900"
+        onClick={() => {
+          setIsInQueue(false);
+        }}
+      >
+        cancel
+      </button>
     </div>
   );
 };
