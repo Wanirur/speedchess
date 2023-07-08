523d8c1b38b832d5abfb0d2bb8bb9d764217d6af
Wanirur <vaniloor@gmail.com>
2023-07-08 18:15:13 +0200

    scroll the latest move into view on moves history panel

--- src/components/moves_history.tsx
+++ src/components/moves_history.tsx
@@ -1,4 +1,10 @@
-import { type HTMLAttributes, type Dispatch, type SetStateAction } from "react";
+import {
+  type HTMLAttributes,
+  type Dispatch,
+  type SetStateAction,
+  useEffect,
+  useRef,
+} from "react";
 import { twMerge } from "tailwind-merge";
 import type Chess from "~/utils/chess";
 
@@ -10,9 +16,17 @@ const MovesHistory: React.FC<
   } & HTMLAttributes<HTMLDivElement>
 > = ({ className, chess, index: indexToSelect, setIndex }) => {
   const moves = chess.algebraic;
+  const lastMoveRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (lastMoveRef.current) {
+      lastMoveRef.current.scrollIntoView();
+    }
+  }, [moves.length]);
 
   return (
     <div
+      ref={lastMoveRef}
       className={twMerge(
         "font-white flex flex-wrap content-start gap-0.5 overflow-y-auto bg-neutral-700 px-2 py-3  font-os text-white",
         className
@@ -30,6 +44,7 @@ const MovesHistory: React.FC<
 
         return (
           <div
+            ref={index === moves.length - 1 ? lastMoveRef : undefined}
             key={index}
             className={twMerge(
               "h-fit w-fit cursor-pointer whitespace-nowrap rounded-sm p-1 hover:bg-neutral-500",
